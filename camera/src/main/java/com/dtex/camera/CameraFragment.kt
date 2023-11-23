7144c81bf216f604f0dfc2eebb6242d9c61c5b90
wenge8n <wenge8n@outlook.com>
2023-11-23 18:54:01 +0800

    Request permissions

--- camera/src/main/java/com/dtex/camera/CameraFragment.kt
+++ camera/src/main/java/com/dtex/camera/CameraFragment.kt
@@ -1,9 +1,15 @@
 package com.dtex.camera
 
+import android.Manifest
+import android.content.pm.PackageManager
+import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import com.dtex.camera.databinding.FragmentCameraBinding
 
@@ -11,6 +17,28 @@ class CameraFragment : Fragment() {
 
     private var _binding: FragmentCameraBinding? = null
     private val binding get() = _binding!!
+
+    private val requestPermissionsLauncher =
+        registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) { permissions ->
+            val isGranted = permissions.entries.all { it.value }
+            if (isGranted) {
+                startCamera()
+            } else {
+                Toast.makeText(requireContext(), "Permission request denied", Toast.LENGTH_SHORT)
+                    .show()
+                // Finish activity
+                activity?.finish()
+            }
+        }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        if (hasPermissions()) {
+            startCamera()
+        } else {
+            requestPermissionsLauncher.launch(REQUIRED_PERMISSIONS)
+        }
+    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -20,8 +48,27 @@ class CameraFragment : Fragment() {
         return binding.root
     }
 
+    private fun hasPermissions() = REQUIRED_PERMISSIONS.all {
+        ContextCompat.checkSelfPermission(requireContext(), it) == PackageManager.PERMISSION_GRANTED
+    }
+
+    private fun startCamera() {
+    }
+
     override fun onDestroyView() {
         super.onDestroyView()
         _binding = null
+    }
+
+    companion object {
+        private val REQUIRED_PERMISSIONS =
+            mutableListOf(
+                Manifest.permission.CAMERA
+            ).apply {
+                if (Build.VERSION.SDK_INT <= Build.VERSION_CODES.P) {
+                    add(Manifest.permission.READ_EXTERNAL_STORAGE)
+                    add(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                }
+            }.toTypedArray()
     }
 }
