a5cb8406d7c9bf4a5a55fcfcff682873d3352866
wenge8n <wenge8n@outlook.com>
2023-11-23 09:52:25 +0800

    Add camera activity

--- app/src/main/java/com/dtex/cameraexample/MainActivity.kt
+++ app/src/main/java/com/dtex/cameraexample/MainActivity.kt
@@ -1,11 +1,32 @@
 package com.dtex.cameraexample
 
-import androidx.appcompat.app.AppCompatActivity
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.appcompat.app.AppCompatActivity
+import com.dtex.camera.CameraActivity
+import com.dtex.cameraexample.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    private val cameraLauncher =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            if (result.resultCode == Activity.RESULT_OK) {
+                val data = result.data
+            }
+        }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.cameraButton.setOnClickListener {
+            val intent = Intent(this, CameraActivity::class.java)
+            cameraLauncher.launch(intent)
+        }
     }
 }
