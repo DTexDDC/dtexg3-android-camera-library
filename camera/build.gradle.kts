344784e41bf6c01ef73c5430f05aa840ee251955
wenge8n <wenge8n@outlook.com>
2023-12-28 18:41:28 +0800

    Bump version

--- camera/build.gradle.kts
+++ camera/build.gradle.kts
@@ -82,7 +82,7 @@ afterEvaluate {
             register<MavenPublication>("release") {
                 groupId = "com.github.DTexDDC"
                 artifactId = "dtexg3-android-camera-library"
-                version = "0.0.1"
+                version = "0.0.2"
 
                 afterEvaluate {
                     from(components["release"])
